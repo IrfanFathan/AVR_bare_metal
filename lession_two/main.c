3fb6be14c17c9b2ee103051c49d553a13ee35cbf
Irfan Fathan M <muhammadirfanvnb@gmail.com>
2026-05-03 11:55:51 +0530

    Refactor main.c for LED segment display and add hex/elf files for lession_two

--- lession_two/main.c
+++ lession_two/main.c
@@ -1,16 +1,18 @@
+#define F_CPU 16000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void) {
-    // Configure PB5 (Arduino pin 13) as output
-    DDRB |= (1 << PB5);
-
-    while (1) {
-        PORTB |= (1 << PB5);   // LED ON
-        _delay_ms(500);
-        PORTB &= ~(1 << PB5);  // LED OFF
-        _delay_ms(500);
-    }
-
-    return 0;
+int main(void)
+{
+   DDRD = 0xFF; // Set all pins on PORTD as output
+   unsigned char segment[10] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F}, i;  
+   while (1)
+   {
+      for (i = 0; i < 10; i++)
+      {
+         PORTD = segment[i];
+         _delay_ms(500);
+      }
+   }
+   return 0;
 }
