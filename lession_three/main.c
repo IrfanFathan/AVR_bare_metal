11e16ae70fb540cbba894e1b4a715f57bc611f30
Irfan Fathan M <muhammadirfanvnb@gmail.com>
2026-05-06 12:50:32 +0530

    Implement LED button control functionality and add hex/elf files for lesson_three

--- lession_three/main.c
+++ lession_three/main.c
@@ -1,15 +1,32 @@
+/*************************************************
+ * Program   : LED Button Control
+ * Author    : Irfan Fathan M
+ * Date      : 06 May 2026
+ * MCU       : ATmega328P (16 MHz)
+ *
+ * Description:
+ * Turns LED on PB0 on/off based on button input on PD0.
+ *************************************************/
+
+#define F_CPU 16000000UL // 16 MHz clock speed
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void) {
-    // Configure PB5 (Arduino pin 13) as output
-    DDRB |= (1 << PB5);
+int main(void)
+{
+    DDRB |= (1 << DDB0);  // Set PB0 as output
+    DDRD &= ~(1 << DDD0); // Set PD0 as input
 
-    while (1) {
-        PORTB |= (1 << PB5);   // LED ON
-        _delay_ms(500);
-        PORTB &= ~(1 << PB5);  // LED OFF
-        _delay_ms(500);
+    while (1)
+    {
+        if (PIND & (1 << PIND0)) // Check if the button connected to PD0 is pressed
+        {
+            PORTB |= (1 << PB0); // Turn on the LED connected to PB0
+        }
+        else
+        {
+            PORTB &= ~(1 << PB0); // Turn off the LED connected to PB0
+        }
     }
 
     return 0;
