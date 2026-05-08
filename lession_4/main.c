a342fd2638437398482906e22acbbe900455e2b8
Irfan Fathan M <muhammadirfanvnb@gmail.com>
2026-05-08 19:47:15 +0530

    Refactor LCD control functions for write operation and add hex/elf files for lesson four

--- lession_4/main.c
+++ lession_4/main.c
@@ -18,8 +18,8 @@ void lcd_data(unsigned char data)
 {
     PORTD = data;        // Place character data on data bus (Port D)
     PORTB |= (1 << PB0); // Set RS high to select data register mode
-    PORTB |= (1 << PB1); // Set RW high for read operation
-    PORTB |= (1 << PB2); // Set E high to enable data latch
+    PORTB &= ~(1 << PB1); // Set RW low for write operation
+    PORTB |= (1 << PB2); // Set E high initially
     _delay_ms(10);
     PORTB &= ~(1 << PB2); // Set E low to complete the write cycle
 }
@@ -28,16 +28,15 @@ void lcd_cmd(unsigned char command)
 {
     PORTD = command;      // Place command byte on data bus (Port D)
     PORTB &= ~(1 << PB0); // Clear RS to select instruction register mode
-    PORTB |= (1 << PB1);  // Set RW high for read operation
-    PORTB |= (1 << PB2);  // Set E high to execute command
+    PORTB &= ~(1 << PB1); // Set RW low for write operation
+    PORTB |= (1 << PB2); // Set E high initially
     _delay_ms(10);
     PORTB &= ~(1 << PB2); // Set E low to complete the write cycle
 }
 
-void lcd_string(unsigned char *str, unsigned char length)
+void lcd_string(const unsigned char* str, unsigned char length)
 {
-    char i = 0;
-    for (i = 0; i < length; i++) // Send each character one by one
+    for (char i = 0; i < length; i++) // Send each character one by one
     {
         lcd_data(str[i]);
     }
@@ -54,7 +53,7 @@ int main(void)
 {
     DDRD = 0xFF;                                     // LCD data bus on Port D
     DDRB |= (1 << DDB0) | (1 << DDB1) | (1 << DDB2); // Control pins on PB0-PB2
-
+    lcd_initiaise();                                 // Initialize LCD settings
     while (1)
     {
         lcd_cmd(0x80);             // Move cursor to first line start
