29da1a7ca8d74f0994eda2ce80f754114facd726
Irfan Fathan M <muhammadirfanvnb@gmail.com>
2026-05-09 21:10:55 +0530

    Refactor main function for improved readability and organization

--- lession_08/main.c
+++ lession_08/main.c
@@ -8,20 +8,22 @@
  * This is a template for an AVR microcontroller project using the ATmega328P.
  *************************************************/
 
-
 #define F_CPU 16000000UL // Define CPU frequency for delay functions
 
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void) {
-    sei();// Enable global interrupts
-       TIMSK1 |=(1<<TOIE1); // Enable Timer1 overflow interrupt
-       
+int main(void)
+{
+    sei();                  // Enable global interrupts
+    TIMSK1 |= (1 << TOIE1); // Enable Timer1 overflow interrupt
 
-    while (1) {
-      
+    // Set Modes of timer1 to normal mode
+    TCCR1A = 0;
+    TCCR1B = 0;
 
+    while (1)
+    {
     }
 
     return 0;
