60d5a31f8a4fb53c966c5218efb0e226db7de09b
Irfan Fathan M <muhammadirfanvnb@gmail.com>
2026-05-22 11:08:27 +0530

    Update program metadata and improve comments in Timer1 Overflow LED Blink example

--- lession_08/main.c
+++ lession_08/main.c
@@ -1,48 +1,53 @@
 /*************************************************
- * Program   : [Project Name]
- * Author    : [Author Name]
- * Date      : [Date]
+ * Program   : Timer1 Overflow LED Blink
+ * Author    : Irfan Fathan
+ * Date      : 22 May 2026
  * MCU       : ATmega328P (16 MHz)
  *
  * Description:
- * This is a template for an AVR microcontroller project using the ATmega328P.
+ * Blink onboard LED using Timer1 overflow
+ * interrupt every 1 second.
  *************************************************/
 
-#define F_CPU 16000000UL // Define CPU frequency for delay functions
+#define F_CPU 16000000UL
 
 #include <avr/io.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
 
+// Timer1 Overflow Interrupt Service Routine
 ISR(TIMER1_OVF_vect)
 {
-    // Reload value for 1 second
+    // Reload timer value for 1 second delay
     TCNT1 = 49911;
 
-    // Toggle LED
+    // Toggle PB5 (Arduino Uno onboard LED)
     PORTB ^= (1 << PORTB5);
 }
 
 int main(void)
 {
-    // enable global interrupt bit
-    sei();
-    // enable required timer interrupt
+    // Set PB5 as output
+    DDRB |= (1 << DDB5);
+
+    // Timer1 Normal Mode
+    TCCR1A = 0;
+    TCCR1B = 0;
+
+    // Preload timer for 1 second
+    TCNT1 = 49911;
+
+    // Set prescaler to 1024
+    TCCR1B |= (1 << CS12) | (1 << CS10);
+
+    // Enable Timer1 Overflow Interrupt
     TIMSK1 |= (1 << TOIE1);
-    // set mode of timer to normal mode
-    TCCR1A &= ~((1 << WGM10) | (1 << WGM11));
-    TCCR1B &= ~((1 << WGM12) | (1 << WGM13));
-    // set prescaler to 1024
-    TCCR1B |= (1 << CS10) | (1 << CS12);
-    TCCR1B &= ~(1 << CS11);
-    // load prefers for it to generate interrupt every 1 second
-    TCNT1 = 0;
-    OCR1A = 49910;       // 16,000,000 / 1024 = 15625
-    DDRB |= (1 << DDB5); // Set PB5 as output (built-in LED on many AVR boards)
+
+    // Enable global interrupts
+    sei();
 
     while (1)
     {
-        // Main loop can be used for other tasks while timer interrupt handles LED toggling
+        // Main loop empty
     }
 
     return 0;
