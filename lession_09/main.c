58ecc5b3b4d49c684c2e70bacbfe77e7087988db
Irfan Fathan M <muhammadirfanvnb@gmail.com>
2026-05-22 11:11:18 +0530

    Add Makefile and main.c template for lesson 09

---
+++ lession_09/main.c
@@ -0,0 +1,24 @@
+/*************************************************
+ * Program   : [Project Name]
+ * Author    : [Author Name]
+ * Date      : [Date]
+ * MCU       : ATmega328P (16 MHz)
+ *
+ * Description:
+ * This is a template for an AVR microcontroller project using the ATmega328P.
+ *************************************************/
+
+
+#define F_CPU 16000000UL // Define CPU frequency for delay functions
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+   
+    while (1) {
+       
+    }
+
+    return 0;
+}
