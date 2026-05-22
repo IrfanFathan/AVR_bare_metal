9222756075e95cfa693e6cbe414cc659d214263d
Irfan Fathan M <muhammadirfanvnb@gmail.com>
2026-05-22 18:30:19 +0530

    Add Makefile and main.c for Timer Interrupt Programming project

---
+++ lession_10/main.c
@@ -0,0 +1,27 @@
+/*************************************************
+ * Program   : Timer Intrrupt Programming
+ * Author    : Irfan fathan
+ * Date      : 2026-05-22
+ * MCU       : ATmega328P @ 16 MHz
+ *
+ * Description:
+ *   [Add description here]
+ *************************************************/
+
+#define F_CPU 16000000UL  /* CPU frequency for delay functions */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+
+    /* Initialization */
+
+    while (1) {
+
+        /* Main loop */
+
+    }
+
+    return 0;
+}
