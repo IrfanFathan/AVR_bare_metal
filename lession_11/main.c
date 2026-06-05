ef559cd571d96daf52c023e4bb339be0ca6db3b8
Irfan Fathan M <muhammadirfanvnb@gmail.com>
2026-06-05 23:04:12 +0530

    Add AVR Forge project generator script and initial project files for lesson 11 and lesson 12

---
+++ lession_11/main.c
@@ -0,0 +1,27 @@
+/*************************************************
+ * Program   : PWM Program
+ * Author    : Irfan Fathan
+ * Date      : 2026-06-05
+ * MCU       : ATmega328P @ 16 MHz
+ *
+ * Description:
+ *   [Add description here]
+ *************************************************/
+
+#define F_CPU 16000000UL  /* CPU frequency for delay functions */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+
+    /* Initialization */
+
+    while (1) {
+
+        /* Main loop */
+
+    }
+
+    return 0;
+}
---
+++ lesson_12/main.c
@@ -0,0 +1,27 @@
+/*************************************************
+ * Program   : df
+ * Author    : —
+ * Date      : 2026-06-05
+ * MCU       : ATmega328P @ 16000000 Hz
+ *
+ * Description:
+ *   [Add description here]
+ *************************************************/
+
+#define F_CPU 16000000UL  /* CPU frequency for delay functions */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+
+    /* Initialization */
+
+    while (1) {
+
+        /* Main loop */
+
+    }
+
+    return 0;
+}
